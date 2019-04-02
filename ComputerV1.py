181f2055ee0b21c2eb80f2d713dcd62fdc23609f
ravard <avardrobin@hotmail.com>
2019-04-02 19:49:20 +0200

    normalement pour la gestion input c est ok

--- ComputerV1.py
+++ ComputerV1.py
@@ -1,6 +1,10 @@
 import sys
 import matplotlib.pyplot as plt
 
+#On considèrera toujours que l’entrée est bien formatée,
+#ie:
+#Tous les termes sont de la forme ap * x^p. 
+#Les puissances sont bien ordonnées et toutes présentes
 
 def printGraph(coef):
 	axes = plt.gca()
@@ -9,6 +13,7 @@ def printGraph(coef):
 	plt.plot([-10, 10], [0, 0], c='black')
 	
 	ylim = max([f(x, coef) for x in range(-10, 11)])
+	ylim = max(ylim, 10)
 	axes.set_ylim(-ylim, ylim)
 	plt.plot([0, 0], [-ylim, ylim], c='black')
 	
@@ -19,29 +24,46 @@ def delta(coef):
 	return (coef[1]**2 - 4*coef[2]*coef[0])
 
 def f(x, coef):
-	return coef[2] * x ** 2 + coef[1] * x + coef[0]
+	ret = 0;
+	for i in range(0, len(coef)):
+		ret += coef[i] * x ** i
+	return ret
 
+def print_forme_reduite(coef):
+	print('Forme réduite de l\'équation: ', end='')
+	for i in range(0, len(coef)):
+		if (i is not len(coef) - 1):
+			end = ' + ' if coef[i + 1] >= 0 else ' - '
+		else:
+			end = " = 0\n"
+		if (coef[i].is_integer()):
+			print('{} * X^{}'.format(abs(int(coef[i])), i), end=end)
+		else:
+			print('{} * X^{}'.format(abs(coef[i]), i), end=end)
 
 if __name__ == '__main__':
 	if (len(sys.argv) != 2):
-		print("usage: python ComputerV1.py 'd * X^0 + e * X^1 + f * X^2 = g * X^0 + h * X^1 + i * X^2'")	
-	else:
-		left = [int(tab.split("*")[0]) for tab in sys.argv[1].split("=")[0].split('+')]
-		right = [int(tab.split("*")[0]) for tab in sys.argv[1].split("=")[1].split('+')]
-		coef = [left[0] - right[0], left[1] - right[1], left[2] - right[2]]
+		sys.exit("usage: python ComputerV1.py 'a0 * X^0 + a1 * X^1 + a2 * X^2 + ... + an * X^n = b0 * X^0 + b1 * X^1 + b2 * X^2 + ... + bm * X^m'")	
 
-
-		print('La forme reduite de l\'equation est la suivante: {} * X^0 + {} * X^1 + {} * X^2 = 0'.format(coef[0], coef[1], coef[2]))
-
-		if (coef[2] != 0):
-			degree = 2
-		else:
-			if (coef[1] != 0):
-				degree = 1
-			else:
-				degree = 0
-
-		print('Polynomial degree:', degree)
-		if (degree == 2):
-			print('discriminant:', delta(coef))
-		printGraph(coef)
+	s = sys.argv[1]
+	
+	while (1):
+		index = s.find('- ')
+		if (index == -1):
+			break
+		s = s[:index] + '+ -' + s[index + 2:]
+		
+	left = [float(tab.split("*")[0]) for tab in s.split("=")[0].split('+')]
+	right = [float(tab.split("*")[0]) for tab in s.split("=")[1].split('+')]
+	n = max(len(left), len(right))
+	left.extend([0 for i in range(0, n - len(left))])
+	right.extend([0 for i in range(0, n - len(right))])
+	coef = [left[i] - right[i] for i in range(0, n)]
+	while (not coef[-1] and len(coef) > 1):
+		del coef[-1]
+	print_forme_reduite(coef)
+	degree = len(coef) - 1
+	print('Degré du polynôme:', degree)
+	if (degree == 2):
+		print('discriminant:', delta(coef))
+	printGraph(coef)
